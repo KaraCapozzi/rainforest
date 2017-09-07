86035f72cc8f8ff8b6090e28c7df7c0023b3ceea
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-07 17:21:43 -0400

    form submit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,21 @@ def index
 end
 
 def create
+  @product = Product.new
+
+  @product.name = params[:product][:name]
+  @product.description = params[:product][:description]
+  @product.price_in_cents = params[:product][:price_in_cents]
+
+  if @product.save
+    redirect_to '/products'
+  else
+    render :new
+  end
 end
 
 def new
+  @product = Product.new
 end
 
 def edit
