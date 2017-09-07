47dff237d6e2927002ed04ce50b12a605ceaae06
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-07 11:43:58 -0400

    add products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name:"ring", description:"shiny, silver, unique", price_in_cents:3000.00)
+Product.create(name:"bracelet", description:"shiny, red, unique", price_in_cents:4000.00)
+Product.create(name:"necklace", description:"shiny, gold, unique", price_in_cents:5000.00)
+Product.create(name:"earrings", description:"shiny, ruby, unique", price_in_cents:6000.00)
