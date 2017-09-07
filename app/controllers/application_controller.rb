87bb28fc8aadc336ad6e2c7e40459f88c53e5cb1
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-07 14:04:35 -0400

    put products on index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
 end
