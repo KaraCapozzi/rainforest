03081f39e9b29913b381c65e2d450a92d709006d
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-08 09:56:04 -0400

    changes in reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,17 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.new(review: params[:review][:review][:id])
 
+    @review.name = params[:review][:name]
+    @review.review = params[:review][:review]
+
+    if @review.save
+      redirect_to '/products/:id'
+    else
+      render :new
+    end
+    flash[:notice] = "Review successfully created"
   end
 
   def new
@@ -9,15 +19,28 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:product_id])
 
+    @review = Review.find(params[:id])
+
+    @review.name = params[:review][:name]
+    @product.review = params[:review][:review]
+
+    if @review.save
+        redirect_to "/products/#{params[:id]}"
+      else
+        render :edit
+      end
   end
 
   def destroy
-
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 
 
