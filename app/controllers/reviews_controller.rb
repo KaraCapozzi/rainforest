6af37fb8077c27112ff56a04d2a11d613266b325
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-07 21:27:06 -0400

    reviews controller and views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+
+  def create
+
+  end
+
+  def new
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
+end
